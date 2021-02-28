4738e3adc743bcefe55cbeb2d1c8edb1674e8f03
nop <575065955@qq.com>
2021-02-28 16:27:44 +0800

    init

--- docker-build.bat
+++ docker-build.bat
@@ -7,3 +7,6 @@ cd %SCRIPT_PATH%
 git archive --format=tar --prefix=screenshot-api-server/ -o latest.tar HEAD
 
 del /f .\latest.tar
+
+docker rmi wuxue107/screenshot-api-server:latest
+docker build -t wuxue107/screenshot-api-server:latest
