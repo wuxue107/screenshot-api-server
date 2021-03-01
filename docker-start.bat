902ed6a7b1ed22ecb8526956d323816fa06d59b7
nop <575065955@qq.com>
2021-03-01 19:36:05 +0800

    init

--- docker-start.bat
+++ docker-start.bat
@@ -4,5 +4,5 @@ set SCRIPT_PATH=%~dp0
 cd %SCRIPT_PATH%
 
 
-docker run -rm -p 3000:3000 -v "%SCRIPT_PATH%/public:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
+docker run --rm -p 3000:3000 -v "%SCRIPT_PATH%/public:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
 
