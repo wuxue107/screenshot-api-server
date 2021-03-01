ccdddb610400e29bd4c12dfe8c5c2fb88b27f78e
nop <575065955@qq.com>
2021-03-01 20:07:34 +0800

    init

--- docker-start.sh
+++ docker-start.sh
@@ -3,4 +3,4 @@
 SCRIPT_PATH=$(cd `dirname "$0"`;pwd)
 cd "${SCRIPT_PATH}";
 
-docker run --rm -p 3000:3000 -v "${SCRIPT_PATH}/public:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
+docker run --rm -td -p 3000:3000 -v "${SCRIPT_PATH}/public:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
