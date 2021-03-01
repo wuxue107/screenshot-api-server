303868882610959847d3256ed4a69cb1b0431940
nop <575065955@qq.com>
2021-03-01 23:18:40 +0800

    init

--- docker-start.bat
+++ docker-start.bat
@@ -6,5 +6,5 @@ set MOUNT_PATH=./public
 
 FOR /F %%i in ('wslpath.bat %MOUNT_PATH%') do ( set MOUNT_PATH=%%i)
 
-
+echo docker run --rm -td -p 3000:3000 -v "%MOUNT_PATH%:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
 docker run --rm -td -p 3000:3000 -v "%MOUNT_PATH%:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
--- docker-start.sh
+++ docker-start.sh
@@ -4,4 +4,5 @@ SCRIPT_PATH=$(cd `dirname "$0"`;pwd)
 cd "${SCRIPT_PATH}"
 MOUNT_PATH=./public
 
+echo docker run --rm -td -p 3000:3000 -v "./public:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
 docker run --rm -td -p 3000:3000 -v "./public:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
