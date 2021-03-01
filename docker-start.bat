65507e0304f3b8bb35d14804b6c77e30570e8928
nop <575065955@qq.com>
2021-03-01 23:43:24 +0800

    init

--- docker-start.bat
+++ docker-start.bat
@@ -5,6 +5,6 @@ cd %SCRIPT_PATH%
 set MOUNT_PATH=./public
 
 FOR /F %%i in ('wslpath.bat %MOUNT_PATH%') do ( set MOUNT_PATH=%%i)
-
-echo docker run --rm -td -p 3000:3000 -v "%MOUNT_PATH%:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
-docker run --rm -td -p 3000:3000 -v "%MOUNT_PATH%:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
+:: warning windows 上无法直接挂载文件夹，虚配置 docker file share
+echo docker run -v "%MOUNT_PATH%:/screenshot-api-server/public" --rm -td -p 3000:3000 --name=screenshot-api-server wuxue107/screenshot-api-server
+docker run -v "%MOUNT_PATH%:/screenshot-api-server/public" --rm -td -p 3000:3000 --name=screenshot-api-server wuxue107/screenshot-api-server
