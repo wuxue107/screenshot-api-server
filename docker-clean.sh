9c52a53a8c70058f65ebc55cb68d3ac7bdb383dc
nop <575065955@qq.com>
2021-03-03 22:26:26 +0800

    init

--- docker-clean.sh
+++ docker-clean.sh
@@ -3,4 +3,6 @@
 SCRIPT_PATH=$(cd `dirname "$0"`;pwd)
 cd "${SCRIPT_PATH}";
 
-docker stop screenshot-api-server && docker rm screenshot-api-server && docker rmi wuxue107/screenshot-api-server 
+docker stop screenshot-api-server 
+docker rm screenshot-api-server 
+docker rmi wuxue107/screenshot-api-server 
