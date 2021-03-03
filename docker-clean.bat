cdd636c8787e0c492b776b659a8542e352db928b
nop <575065955@qq.com>
2021-03-03 22:27:32 +0800

    init

--- docker-clean.bat
+++ docker-clean.bat
@@ -3,4 +3,6 @@
 set SCRIPT_PATH=%~dp0
 cd %SCRIPT_PATH%
 
-docker stop screenshot-api-server && docker rm screenshot-api-server && docker rmi wuxue107/screenshot-api-server 
+docker stop screenshot-api-server 
+docker rm screenshot-api-server 
+docker rmi wuxue107/screenshot-api-server 
