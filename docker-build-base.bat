98c429a75ca7eead06ee274d7a0ea4bcaa21ea1f
nop <575065955@qq.com>
2021-03-04 14:33:45 +0800

    remove dockerfile base

--- docker-build-base.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set SCRIPT_PATH=%~dp0
-cd %SCRIPT_PATH%
-
-docker rmi wuxue107/screenshot-api-server-base:latest
-docker build -t wuxue107/screenshot-api-server-base:latest -f ./DockerfileBase
-
