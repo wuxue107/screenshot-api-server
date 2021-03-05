8dda56dda871a0766333e8b6b462a7f0460d10ac
nop <575065955@qq.com>
2021-03-05 15:16:01 +0800

    download from safari

--- docker-start.sh
+++ docker-start.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_PATH=$(cd `dirname "$0"`;pwd)
 cd "${SCRIPT_PATH}"
-MOUNT_PATH=./public
+MOUNT_PATH="${PWD}/public"
 
-echo docker run --rm -td -p 3000:3000 -v "./public:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
-docker run --rm -td -p 3000:3000 -v "./public:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
+echo docker run --rm -td -p 3000:3000 -v "${MOUNT_PATH}:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
+docker run --rm -td -p 3000:3000 -v "${MOUNT_PATH}:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
