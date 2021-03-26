e3e24512b8c29204cb14f9a015a39f7a69d56767
nop <575065955@qq.com>
2021-03-27 04:18:05 +0800

    add .gitattributes

--- docker-build.bat
+++ docker-build.bat
@@ -4,7 +4,7 @@ set SCRIPT_PATH=%~dp0
 cd %SCRIPT_PATH%
 
 
-git archive --format=tar --prefix=screenshot-api-server/ -o latest.tar HEAD
+git archive --format=tar --worktree-attributes --prefix=screenshot-api-server/ -o latest.tar HEAD
 
 docker rmi wuxue107/screenshot-api-server:latest
 docker build -t wuxue107/screenshot-api-server:latest .
--- docker-build.sh
+++ docker-build.sh
@@ -4,7 +4,7 @@ SCRIPT_PATH=$(cd `dirname "$0"`;pwd)
 cd "${SCRIPT_PATH}";
 
 # DOCKER_IMAGE_VERSION=$(node --eval="console.log(require('./package.json').version)")
-git archive --format=tar --prefix=screenshot-api-server/ -o latest.tar HEAD
+git archive --format=tar --worktree-attributes --prefix=screenshot-api-server/ -o latest.tar HEAD
 
 docker rmi wuxue107/screenshot-api-server:latest
 docker build -t wuxue107/screenshot-api-server:latest .
