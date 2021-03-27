0a498856c8e1c1164e80658ce518af072e2a77e6
nop <575065955@qq.com>
2021-03-27 17:35:30 +0800

    ignore wslpath error

--- docker-start.bat
+++ docker-start.bat
@@ -4,7 +4,7 @@ set SCRIPT_PATH=%~dp0
 cd %SCRIPT_PATH%
 set MOUNT_PATH=./public
 
-call wslpath.bat "%MOUNT_PATH%" MOUNT_PATH
+call wslpath.bat "%MOUNT_PATH%" MOUNT_PATH 2>NUL
 
 echo docker run -v "%MOUNT_PATH%:/screenshot-api-server/public" --rm -td -p 3000:3000 --name=screenshot-api-server wuxue107/screenshot-api-server
 docker run -v "%MOUNT_PATH%:/screenshot-api-server/public" --rm -td -p 3000:3000 --name=screenshot-api-server wuxue107/screenshot-api-server
