4c865c498a318ccd72ec9f47b47ae8d9fbc2d04a
wuxue107 <575065955@qq.com>
2021-07-15 21:12:04 +0800

    update bookjs

--- docker-start.bat
+++ docker-start.bat
@@ -4,6 +4,8 @@ set SCRIPT_PATH=%~dp0
 cd %SCRIPT_PATH%
 set MOUNT_PATH=./public
 
+if exist "%MOUNT_PATH%" mkdir "%MOUNT_PATH%"
+
 call wslpath.bat "%MOUNT_PATH%" MOUNT_PATH 2>NUL
 
 echo docker run -v "%MOUNT_PATH%:/screenshot-api-server/public" --rm -td -p 3000:3000 --name=screenshot-api-server wuxue107/screenshot-api-server
--- docker-start.sh
+++ docker-start.sh
@@ -4,5 +4,7 @@ SCRIPT_PATH=$(cd `dirname "$0"`;pwd)
 cd "${SCRIPT_PATH}"
 MOUNT_PATH="${PWD}/public"
 
+[ -d "${MOUNT_PATH}" ] || mkdir -p "${MOUNT_PATH}"
+
 echo docker run --rm -td -p 3000:3000 -v "${MOUNT_PATH}:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
 docker run --rm -td -p 3000:3000 -v "${MOUNT_PATH}:/screenshot-api-server/public" --name=screenshot-api-server wuxue107/screenshot-api-server
