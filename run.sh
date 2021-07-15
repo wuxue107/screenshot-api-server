f376876ea3591781863a066a53918d95ead479f8
wuxue107 <575065955@qq.com>
2021-07-15 20:42:39 +0800

    update bookjs

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 SCRIPT_PATH=$(cd `dirname "$0"`;pwd)
 cd "${SCRIPT_PATH}";
 
+[ -d ./public/fonts ] || mkdir -p ./public/fonts
+[ -d ./public/pdf ] || mkdir -p ./public/pdf
+
 chmod 755 ./install-font.sh && ./install-font.sh
 
 yarn start
