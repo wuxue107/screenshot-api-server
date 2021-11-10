3079c267348c00841daf4b740771579d8d9eee30
wuxue107 <575065955@qq.com>
2021-11-11 00:30:28 +0800

    update bookjs

--- update-bookjs.sh
+++ update-bookjs.sh
@@ -4,7 +4,7 @@ SCRIPT_PATH=$(cd `dirname "$0"`;pwd)
 cd "${SCRIPT_PATH}";
 
 echo #################### UPDATE BOOKJS-EAZY #####################
-wget https://gitee.com/wuxue107/bookjs-eazy/raw/master/dist/static/js/bookjs/latest/bookjs-eazy.min.js -O /screenshot-api-server/static/js/bookjs/latest/bookjs-eazy.min.js
+wget https://gitee.com/wuxue107/bookjs-eazy/raw/master/dist/static/js/bookjs/latest/bookjs-eazy.min.js -O ./static/js/bookjs/latest/bookjs-eazy.min.js
 
 echo ####################  UPDATE BOOKJS-EAZY  #####################
 
