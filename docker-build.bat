d1e1d61d5db9897ff3e62c8bb7756286cec77d17
wuxue107 <575065955@qq.com>
2022-09-02 23:03:03 +0800

    bookjs-eazy 1.14.0

--- docker-build.bat
+++ docker-build.bat
@@ -3,12 +3,15 @@
 set SCRIPT_PATH=%~dp0
 cd %SCRIPT_PATH%
 
+node --eval="console.log(require('./package').version);" >version.tmp
+set /p VERSION=<version.tmp
+del /f version.tmp
 
 if "%1" == "" (
     git archive --format=tar --worktree-attributes --prefix=screenshot-api-server/ -o latest.tar HEAD
     
-    docker rmi wuxue107/screenshot-api-server:latest
-    docker build -f Dockerfile -t wuxue107/screenshot-api-server:latest .
+    docker rmi wuxue107/screenshot-api-server:%VERSION%
+    docker build -f Dockerfile -t wuxue107/screenshot-api-server:%VERSION% .
     del /f latest.tar
 )
 
