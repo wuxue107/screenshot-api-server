6b32707c5187aa320ca713ca3da2656de9d57cea
nop <575065955@qq.com>
2023-10-15 00:20:49 +0800

    更新puppeteer

--- docker-build.bat
+++ docker-build.bat
@@ -8,17 +8,14 @@ node --eval="console.log(require('./package').version);" >version.tmp
 set /p IMAGE_VERSION=<version.tmp
 del /f version.tmp
 
-if "%1" == "" (
-    git archive --format=tar --worktree-attributes --prefix=screenshot-api-server/ -o latest.tar HEAD
-    
-    docker rmi wuxue107/screenshot-api-server:%IMAGE_VERSION%
-    docker build -f Dockerfile -t wuxue107/screenshot-api-server:%IMAGE_VERSION% .
-    del /f latest.tar
-    
-    docker rmi wuxue107/screenshot-api-server:latest
-    docker tag wuxue107/screenshot-api-server:%IMAGE_VERSION% wuxue107/screenshot-api-server:latest
+
+:: 包含nodejs、wkhtmlpdf、chrome所需依赖、jdk
+if "%1" == "base" (
+    docker rmi wuxue107/screenshot-api-base:latest
+    docker  build -f Dockerfile-Base -t wuxue107/screenshot-api-base:%BASE_IMAGE_VERSION% .
 )
 
+:: 包含在base镜像的基础上添加: screenshot-api-server的nodejs依赖和puppeteer chrome
 if "%1" == "fastbase" (
     git archive --format=tar --worktree-attributes --prefix=screenshot-api-server/ -o latest.tar HEAD
     
@@ -27,6 +24,7 @@ if "%1" == "fastbase" (
     del /f latest.tar
 )
 
+:: 在fastbase镜像基础上，将当前screenshot-api-server程序包拷贝进去
 if "%1" == "fast" (
     git archive --format=tar --worktree-attributes --prefix=screenshot-api-server/ -o latest.tar HEAD
     
@@ -38,15 +36,14 @@ if "%1" == "fast" (
     docker tag wuxue107/screenshot-api-server:%IMAGE_VERSION% wuxue107/screenshot-api-server:latest
 )
 
-if "%1" == "base" (
-    docker rmi wuxue107/screenshot-api-base:latest
-    docker  build -f Dockerfile-Base -t wuxue107/screenshot-api-base:%BASE_IMAGE_VERSION% .
-)
-
-if "%1" == "full" (
+:: 包含在base镜像的基础上, 相当于 fastbase + fast
+if "%1" == "" (
     git archive --format=tar --worktree-attributes --prefix=screenshot-api-server/ -o latest.tar HEAD
     
-    docker rmi wuxue107/screenshot-api-server:latest
-    docker build -f Dockerfile-Full -t wuxue107/screenshot-api-server:latest .
+    docker rmi wuxue107/screenshot-api-server:%IMAGE_VERSION%
+    docker build -f Dockerfile -t wuxue107/screenshot-api-server:%IMAGE_VERSION% .
     del /f latest.tar
+    
+    docker rmi wuxue107/screenshot-api-server:latest
+    docker tag wuxue107/screenshot-api-server:%IMAGE_VERSION% wuxue107/screenshot-api-server:latest
 )
--- docker-build.sh
+++ docker-build.sh
@@ -6,17 +6,13 @@ BASE_IMAGE_VERSION=1.3.0
 
 IMAGE_VERSION=$(env node --eval="console.log(require('./package.json').version)")
 
-if [ "$1" == "" ]; then
-    git archive --format=tar --worktree-attributes --prefix=screenshot-api-server/ -o latest.tar HEAD
-    docker rmi wuxue107/screenshot-api-server:${IMAGE_VERSION}
-    docker build -f Dockerfile -t wuxue107/screenshot-api-server:${IMAGE_VERSION} .
-    rm -rf latest.tar
+## 包含nodejs、wkhtmlpdf、chrome所需依赖、jdk
+if [ "$1" == "base" ]; then
+    docker rmi wuxue107/screenshot-api-base:latest
+    docker  build -f Dockerfile-Base -t wuxue107/screenshot-api-base:${BASE_IMAGE_VERSION} .
+fi 
 
-    docker rmi wuxue107/screenshot-api-server:latest
-    docker tag wuxue107/screenshot-api-server:${IMAGE_VERSION} wuxue107/screenshot-api-server:latest
-fi
-
-
+## 包含在base镜像的基础上添加: screenshot-api-server的nodejs依赖和puppeteer chrome
 if [ "$1" == "fastbase" ]; then
     git archive --format=tar --worktree-attributes --prefix=screenshot-api-server/ -o latest.tar HEAD
     
@@ -25,6 +21,7 @@ if [ "$1" == "fastbase" ]; then
     del /f latest.tar
 fi 
 
+## 在fastbase镜像基础上，将当前screenshot-api-server程序包拷贝进去
 if [ "$1" == "fast" ]; then
     git archive --format=tar --worktree-attributes --prefix=screenshot-api-server/ -o latest.tar HEAD
     
@@ -36,17 +33,13 @@ if [ "$1" == "fast" ]; then
     docker tag wuxue107/screenshot-api-server:${IMAGE_VERSION} wuxue107/screenshot-api-server:latest
 fi 
 
-if [ "$1" == "full" ]; then
+## 包含在base镜像的基础上, 相当于 fastbase + fast
+if [ "$1" == "" ]; then
     git archive --format=tar --worktree-attributes --prefix=screenshot-api-server/ -o latest.tar HEAD
-    
+    docker rmi wuxue107/screenshot-api-server:${IMAGE_VERSION}
+    docker build -f Dockerfile -t wuxue107/screenshot-api-server:${IMAGE_VERSION} .
+    rm -rf latest.tar
+
     docker rmi wuxue107/screenshot-api-server:latest
-    docker build -f Dockerfile-Full -t wuxue107/screenshot-api-server:latest .
-    del /f latest.tar
-fi 
-
-if [ "$1" == "base" ]; then
-    docker rmi wuxue107/screenshot-api-base:latest
-    docker  build -f Dockerfile-Base -t wuxue107/screenshot-api-base:${BASE_IMAGE_VERSION} .
-fi 
-
-    
+    docker tag wuxue107/screenshot-api-server:${IMAGE_VERSION} wuxue107/screenshot-api-server:latest
+fi
